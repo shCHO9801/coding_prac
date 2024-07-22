aedc1270befe05f505bfb15f449f7879578dbb8e
shCHO9801 <csh980116@naver.com>
2024-07-22 19:19:31 +0900

    [Bronze III] Title: 알람 시계, Time: 244 ms, Memory: 18512 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+        m-=45;
+        if(m<0){
+            m+=60;
+            h--;
+        }
+        if(h<0){
+            h+=24;
+        }
+        System.out.println(h+" "+m);
+    }
+}
