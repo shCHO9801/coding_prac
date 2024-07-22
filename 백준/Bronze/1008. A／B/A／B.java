9d54defda3b627cc9f2eda7ab556a2645163a11a
shCHO9801 <csh980116@naver.com>
2024-07-23 00:35:43 +0900

    [Bronze V] Title: A/B, Time: 216 ms, Memory: 18020 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -4,6 +4,6 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         double a = sc.nextDouble();
         double b = sc.nextDouble();
-        System.out.println(a/b);
+        System.out.println(a/b);        
     }
 }
