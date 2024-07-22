ddbf15d6a7bb868703f55b6eed83b2d90de43bb7
shCHO9801 <csh980116@naver.com>
2024-07-22 19:22:39 +0900

    [Bronze IV] Title: 주사위 세개, Time: 204 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] dice = new int[3];
+        for(int i=0; i<3; i++){
+            dice[i] = sc.nextInt();
+        }
+        Arrays.sort(dice);
+        if(dice[0]==dice[1]&&dice[1]==dice[2]){
+            System.out.println(10000+dice[0]*1000);
+        }
+        else if(dice[0]==dice[1]||dice[1]==dice[2]){
+            System.out.println(1000+dice[1]*100);
+        }
+        else System.out.println(dice[2]*100);
+    }
+}
