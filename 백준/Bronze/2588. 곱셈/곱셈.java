82f9d3dd4044906e6b4d45e19aebeb62370fae60
shCHO9801 <csh980116@naver.com>
2024-07-22 18:58:30 +0900

    [Bronze III] Title: 곱셈, Time: 208 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int[] answer = new int[4];
+        answer[3] = a*b;
+        answer[0] = b%10 * a;
+        b/=10;
+        answer[1] = b%10 * a;
+        b/=10;
+        answer[2] = b%10 * a;
+        
+        for(int v : answer)
+            System.out.println(v);
+    }
+}
