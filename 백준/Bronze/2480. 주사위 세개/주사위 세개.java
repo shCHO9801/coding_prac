82cbf5e2ef1a6774b3a3ab423a472ccdc40c3e1a
shCHO9801 <csh980116@naver.com>
2024-07-23 01:03:09 +0900

    [Bronze IV] Title: 주사위 세개, Time: 208 ms, Memory: 17692 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -3,16 +3,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] dice = new int[3];
-        for(int i=0; i<3; i++){
+        for(int i=0;i<3;i++)
             dice[i] = sc.nextInt();
-        }
         Arrays.sort(dice);
-        if(dice[0]==dice[1]&&dice[1]==dice[2]){
-            System.out.println(10000+dice[0]*1000);
-        }
-        else if(dice[0]==dice[1]||dice[1]==dice[2]){
-            System.out.println(1000+dice[1]*100);
-        }
-        else System.out.println(dice[2]*100);
+        if(dice[0]==dice[1] && dice[1] == dice[2])
+        System.out.println(10000 + dice[1] * 1000);
+        else if(dice[0]==dice[1] || dice[1]==dice[2])
+            System.out.println(1000 + dice[1] * 100);
+        else
+            System.out.println(dice[2] * 100);
     }
 }
