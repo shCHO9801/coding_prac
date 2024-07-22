04bab02687272fae1138ab2328cf41778abf2346
shCHO9801 <csh980116@naver.com>
2024-07-23 00:54:43 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 216 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -1,13 +1,11 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        String s = "";
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        if(a==b) s = "==";
-        if(a>b) s = ">";
-        if(a<b) s = "<";
-        System.out.println(s);
+        if(a==b) System.out.println("==");
+        if(a>b) System.out.println(">");
+        if(a<b) System.out.println("<");
     }
 }
