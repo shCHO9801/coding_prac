d2758ca555c0d9d0bb5081a6472ffdfcd561d43b
shCHO9801 <csh980116@naver.com>
2024-07-23 00:37:56 +0900

    [Bronze V] Title: ??!, Time: 212 ms, Memory: 17684 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
-        System.out.print(s+"??!");
+        System.out.println(s+"??!");  
     }
 }
