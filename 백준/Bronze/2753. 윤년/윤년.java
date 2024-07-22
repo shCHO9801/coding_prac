81e0889bc0b44e0614fe3a0c026359ed61099252
shCHO9801 <csh980116@naver.com>
2024-07-23 00:57:57 +0900

    [Bronze V] Title: 윤년, Time: 212 ms, Memory: 17744 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -2,9 +2,8 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int answer = 0;
         int year = sc.nextInt();
-        if(year%4==0 && year%100!=0 || year%400==0) answer = 1;
-        System.out.println(answer);
+        if(year%4==0 && year%100!=0 || year%400==0) System.out.println("1");
+        else System.out.println("0");
     }
 }
