e47764daa8825da6b673820773b7cbecdde93b91
shCHO9801 <csh980116@naver.com>
2024-07-24 00:19:26 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 248 ms, Memory: 18684 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,30 +1,35 @@
 import java.util.*;
 public class Main {
+    public static boolean check(String s){
+        String checker = "";
+        int i = 0;
+        char cur = '1';
+        for (; i < s.length(); i++) {
+            char tmp = s.charAt(i);
+            if(i==0)
+                cur = tmp;
+            else{
+                if(cur == tmp)
+                    continue;
+                else{
+                    if(checker.contains(tmp+""))
+                        break;
+                    else cur = tmp;
+                }
+            }
+            checker += cur;
+        }
+        if(i == s.length())
+            return true;
+        return false;
+    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String s;
         int n = in.nextInt(), answer = 0;
         while(n-->0){
-            String checker = "";
             s = in.next();
-            int i = 0;
-            char cur = '1';
-            for (; i < s.length(); i++) {
-                char tmp = s.charAt(i);
-                if(i==0)
-                    cur = tmp;
-                else{
-                    if(cur == tmp)
-                        continue;
-                    else{
-                        if(checker.contains(tmp+""))
-                            break;
-                        else cur = tmp;
-                    }
-                }
-                checker += cur;
-            }
-            if(i == s.length())
+            if(check(s))
                 answer++;
         }
         System.out.println(answer);
