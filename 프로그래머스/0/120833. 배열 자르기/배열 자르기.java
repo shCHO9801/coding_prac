ce2c781c972015393a0bb685ff003f427925899f
shCHO9801 <csh980116@naver.com>
2024-07-24 01:42:53 +0900

    [level 0] Title: 배열 자르기, Time: 3.43 ms, Memory: 98 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        List <Integer> list = new ArrayList<>();
+        for(int i=num1;i<=num2;i++) list.add(numbers[i]);
+        return list.stream().mapToInt(i->i).toArray();
+    }
+}
