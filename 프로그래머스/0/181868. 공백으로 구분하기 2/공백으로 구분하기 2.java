058a05ab478a26ef9550af05a1d6fee9c578b03e
shCHO9801 <csh980116@naver.com>
2024-07-24 02:53:05 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.46 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String my_string) {
+        List<String> list = new ArrayList<>();
+        for(String s : my_string.split(" "))
+            if(!s.equals("")) list.add(s);
+        return list.toArray(new String[0]);
+    }
+}
