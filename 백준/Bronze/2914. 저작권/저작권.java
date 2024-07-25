30e071acfa45d38b716c2fa9388543cfce13d077
shCHO9801 <csh980116@naver.com>
2024-07-25 23:48:15 +0900

    [Bronze III] Title: 저작권, Time: 204 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/2914. 저작권/저작권.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+    static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        int A = in.nextInt();
+        int I = in.nextInt();
+        System.out.println(A*(I-1)+1);
+    }
+}
