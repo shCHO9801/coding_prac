d79d93df10f5359ea1a49d3de7eef28380337431
shCHO9801 <csh980116@naver.com>
2024-07-25 23:17:59 +0900

    [Bronze V] Title: 등록, Time: 160 ms, Memory: 17232 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main {
+    static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println(103);
+        System.out.println("sh980116");
+    }
+}
