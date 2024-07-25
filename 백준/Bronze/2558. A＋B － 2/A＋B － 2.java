091444b9cc9fcef033be006a52319d3f788e75ee
shCHO9801 <csh980116@naver.com>
2024-07-25 23:03:23 +0900

    [Bronze V] Title: A+B - 2, Time: 208 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+    static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println(a+b);
+    }
+}
