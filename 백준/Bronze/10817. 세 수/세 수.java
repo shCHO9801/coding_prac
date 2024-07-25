b211014db1df0c8f60d3375485ded192fa87913e
shCHO9801 <csh980116@naver.com>
2024-07-26 00:23:34 +0900

    [Bronze III] Title: 세 수, Time: 212 ms, Memory: 17792 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+    static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        int[] arr = new int[3];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = in.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[1]);
+    }
+}
