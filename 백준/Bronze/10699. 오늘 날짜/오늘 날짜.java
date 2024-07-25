ea8ac9900cf16de4b00991306415aeb58bb1577e
shCHO9801 <csh980116@naver.com>
2024-07-25 23:16:37 +0900

    [Bronze V] Title: 오늘 날짜, Time: 232 ms, Memory: 18376 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.time.*;
+public class Main {
+    static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+    }
+}
