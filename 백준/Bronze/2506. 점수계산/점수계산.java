b76d8b67a61254da5c2829f2d35bc477ab24766f
shCHO9801 <csh980116@naver.com>
2024-07-27 16:32:07 +0900

    [Bronze III] Title: 점수계산, Time: 124 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/2506. 점수계산/점수계산.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int answer = 0;
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int score = 1;
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            if(x == 0){
+                score = 1;
+            }
+            else{
+                answer+=score++;
+            }
+        }
+        System.out.println(answer);
+    }
+}
