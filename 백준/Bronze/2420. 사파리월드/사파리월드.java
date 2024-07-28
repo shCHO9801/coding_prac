d3cb9609ca42f16611bdbe1b718eb35529544bdd
shCHO9801 <csh980116@naver.com>
2024-07-28 23:46:39 +0900

    [Bronze V] Title: 사파리월드, Time: 184 ms, Memory: 17532 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+public class Main{
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        System.out.println(Math.abs(a-b));
+    }
+}
