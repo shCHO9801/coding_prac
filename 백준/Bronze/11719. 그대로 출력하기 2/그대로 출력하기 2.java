1512484328a36d1808eac67c99925608c2feaa14
shCHO9801 <csh980116@naver.com>
2024-07-28 19:52:34 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 240 ms, Memory: 17932 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+public class Main{
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()){
+            System.out.println(sc.nextLine());
+        }
+    }
+}
