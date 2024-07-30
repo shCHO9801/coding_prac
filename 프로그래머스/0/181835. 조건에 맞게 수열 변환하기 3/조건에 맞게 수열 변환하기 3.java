4665ec1af1b487644dc97bbd862e2b6b33f7c2b1
shCHO9801 <csh980116@naver.com>
2024-07-31 01:07:00 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 6.01 ms, Memory: 163 MB -BaekjoonHub

---
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int[] solution(int[] arr, int k) {
+        for(int i = 0; i < arr.length; i++) 
+            arr[i] = k%2 == 0 ? arr[i] + k : arr[i] * k;
+        return arr;
+    }
+}
