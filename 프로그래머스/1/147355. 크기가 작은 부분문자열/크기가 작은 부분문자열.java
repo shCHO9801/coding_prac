79ba913661f3c0f5d7b0e2252e9378c085006e3c
shCHO9801 <csh980116@naver.com>
2024-08-10 15:48:23 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 2.03 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -1,13 +1,9 @@
-import java.math.*;
 class Solution {
     public int solution(String t, String p) {
         int answer = 0;
-        BigInteger a;
-        BigInteger b = new BigInteger(p);
         for(int i = 0; i < t.length() - p.length() + 1; i++){
             String tmp = t.substring(i, i+p.length());
-            a = new BigInteger(tmp);
-            if(a.compareTo(b) == -1 || a.compareTo(b) == 0) answer++;
+            if(p.compareTo(tmp) >= 0) answer++;
         }
         return answer;
     }
