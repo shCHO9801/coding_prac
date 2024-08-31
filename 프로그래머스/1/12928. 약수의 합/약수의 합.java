b202a5a9c8b388535358262944aac8ea1f8e45dd
shCHO9801 <csh980116@naver.com>
2024-09-01 02:10:35 +0900

    [level 1] Title: 약수의 합, Time: 2.94 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -1,9 +1,8 @@
+import java.util.stream.IntStream;
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        for(int i = 1; i <= n; i++){
-            if(n%i==0) answer+=i;
-        }
-        return answer;
+        return IntStream.range(1, n + 1)
+            .filter(i -> n % i == 0)
+            .sum();
     }
 }
