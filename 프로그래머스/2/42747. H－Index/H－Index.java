df20677b744a059f338b1cf864949c9f64dda580
shCHO9801 <csh980116@naver.com>
2024-09-02 21:07:11 +0900

    [level 2] Title: H-Index, Time: 0.91 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,18 +1,20 @@
 import java.util.*;
 class Solution {
     public int solution(int[] arr) {
+        int answer = arr.length;
+        
         Arrays.sort(arr);
-    
+        
         for(int i = 0; i < arr.length; i++){
-            System.out.println(arr.length - i + " " + arr[i]);
-            if(arr.length - i <= arr[i]){
-                return arr.length - i;
+            if(answer <= arr[i]){
+                break;
             }
+            answer -= 1;
         }
         
         
         // 0 1 3 5 6
-        return 0;
+        return answer;
     }
 }
 
