a6fe0074dd35b3cc03a546661c670ca73266e867
shCHO9801 <csh980116@naver.com>
2024-09-06 23:51:05 +0900

    [Bronze I] Title: 평균, Time: 208 ms, Memory: 18732 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -4,19 +4,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int M = Integer.MIN_VALUE;
-        int[] arr = new int[N];
-        float average = 0;
+        int[] A = new int[N];
 
         for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
-            M = Math.max(M, arr[i]);
+            A[i] = sc.nextInt();
         }
 
-        for(int i : arr){
-            average += (float) i / M * 100;
+        long sum = 0;
+        long max = 0;
+        for (int i = 0; i < N; i++) {
+            if(A[i] > max) max = A[i];
+            sum = sum + A[i];
         }
 
-        System.out.println(average / N);
+        System.out.println(sum * 100.0 / max / N);
     }
 }
