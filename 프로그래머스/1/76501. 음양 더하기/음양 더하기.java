ac7447da3817c78ce4c031c4538c3851978fd326
shCHO9801 <csh980116@naver.com>
2024-09-09 02:36:00 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -1,9 +1,8 @@
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
-        for(int i = 0; i < absolutes.length; i++){
-            if(signs[i]) answer+=absolutes[i];
-            else answer+= -absolutes[i];
+        for(int i = 0; i < signs.length; i++){
+            answer += signs[i] ? absolutes[i] : -absolutes[i];
         }
         return answer;
     }
