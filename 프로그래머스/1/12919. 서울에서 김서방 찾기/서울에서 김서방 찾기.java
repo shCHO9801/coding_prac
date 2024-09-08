c8b33088784b27b8bb72a46c9890e93d0f02337a
shCHO9801 <csh980116@naver.com>
2024-09-09 02:29:16 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 8.53 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,8 +1,6 @@
 import java.util.*;
 class Solution {
     public String solution(String[] seoul) {
-        List <String> list = new ArrayList<>(Arrays.asList(seoul));
-        String find = "Kim";
-        return String.format("김서방은 %d에 있다", list.indexOf(find));
+        return "김서방은 " + Arrays.asList(seoul).indexOf("Kim") + "에 있다";
     }
 }
