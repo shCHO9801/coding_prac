38fc48ae08c2eca8da4ac8535519e1defc79c171
shCHO9801 <csh980116@naver.com>
2024-09-09 02:10:53 +0900

    [level 0] Title: 주사위 게임 3, Time: 1.88 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    public static int[] makeDice(int a, int b, int c, int d){
+        int[] dd = new int[4];
+        
+        dd[0] = a;
+        dd[1] = b;
+        dd[2] = c;
+        dd[3] = d;
+        
+        Arrays.sort(dd);
+        
+        return dd;
+    }
+    public int solution(int a, int b, int c, int d) {
+        int answer = 0;
+        int[] dd = makeDice(a, b, c, d);
+        
+        if(dd[0] == dd[3]){
+            return 1111 * dd[3];
+        }
+        if(dd[1] == dd[3]){
+            return (int)Math.pow((10 * dd[3] + dd[0]), 2);
+        }
+        if(dd[0] == dd[2]){
+            return (int)Math.pow((10 * dd[0] + dd[3]), 2);
+        }
+        if(dd[0] == dd[1] && dd[2] == dd[3]){
+            return (dd[0] + dd[2]) * Math.abs(dd[0] - dd[2]);
+        }
+        if(dd[0] == dd[1]){
+            return dd[2] * dd[3];
+        }
+        if(dd[1] == dd[2]){
+            return dd[0] * dd[3];
+        }
+        if(dd[2] == dd[3]){
+            return dd[0] * dd[1];
+        }
+        
+        return dd[0];
+    }
+}
