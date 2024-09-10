25fc10f461bf42791856f4094b08c1905774bcbd
shCHO9801 <csh980116@naver.com>
2024-09-11 00:25:11 +0900

    [level 2] Title: 피보나치 수, Time: 2.47 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/README.md
+++ 프로그래머스/2/12945. 피보나치 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피보나치 수 - 12945 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945?language=java) 
 
 ### 성능 요약
 
-메모리: 77.8 MB, 시간: 3.99 ms
+메모리: 75.4 MB, 시간: 2.47 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 13일 00:36:37
+2024년 09월 11일 00:25:06
 
 ### 문제 설명
 
--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,12 +1,15 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int[] fibo = new int[100001];
-        if(n < 3) return 1;
-        fibo[1] = fibo[2] = 1;
-        for(int i = 3; i <= n ; i++){
-            fibo[i] = (fibo[i-2] + fibo[i-1]) % 1234567;
+        if(n == 1 || n == 2){
+            return 1;
         }
-        return fibo[n];
+        int[] arr = new int[n + 1];
+        arr[1] = arr[2] = 1;
+        
+        for(int i = 3; i <= n; i++){
+            arr[i] = (arr[i - 1] + arr[i - 2]) % 1234567;
+        }
+        return arr[n];
     }
 }
