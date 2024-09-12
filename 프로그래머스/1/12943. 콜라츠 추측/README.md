b4fa110efbc6e579948de2a961988910851051c6
shCHO9801 <csh980116@naver.com>
2024-09-13 00:32:57 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.12 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/README.md
+++ 프로그래머스/1/12943. 콜라츠 추측/README.md
@@ -1,10 +1,10 @@
 # [level 1] 콜라츠 추측 - 12943 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12943) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12943?language=java) 
 
 ### 성능 요약
 
-메모리: 79.6 MB, 시간: 0.12 ms
+메모리: 81.3 MB, 시간: 0.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 09일 02:34:05
+2024년 09월 13일 00:32:48
 
 ### 문제 설명
 
--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,13 +1,17 @@
 class Solution {
-    
-    public static int f(long n){
+    public static int collatz(long n){
         if(n == 1){
             return 0;
         }
-        return 1 + f(n % 2 == 0 ? n / 2 : (3 * n + 1));
+        
+        if(n % 2 == 0){
+            return 1 + collatz(n/2);
+        } else{
+            return 1 + collatz(3 * n + 1);
+        }
     }
     public int solution(int num) {
-        int answer = f(num);
+        int answer = collatz(num);
         return answer >= 500 ? -1 : answer;
     }
 }
