929679c448cfc419238db3a16c39ab1843c5afbc
shCHO9801 <csh980116@naver.com>
2024-09-16 02:47:09 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 65.8 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -1,11 +1,8 @@
 class Solution {
     public String solution(String s) {
-        String answer = "";
-        int middle = s.length()/2;
-        if(s.length()%2==1)
-            answer = s.substring(middle,middle+1);
-        else
-            answer = s.substring(middle-1,middle+1);
-        return answer;
+        int right = s.length() / 2;
+        int left = s.length() % 2 == 0 ? right - 1 : right;
+        
+        return s.substring(left, right + 1);
     }
 }
