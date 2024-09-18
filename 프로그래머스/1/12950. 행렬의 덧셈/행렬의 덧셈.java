77a13d1eb23c0f4052bc0e3cf0982f6b60b0e074
shCHO9801 <csh980116@naver.com>
2024-09-18 19:00:45 +0900

    [level 1] Title: 행렬의 덧셈, Time: 4.49 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
@@ -1,11 +1,13 @@
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = arr1;
-        for(int i = 0; i < answer.length; i++){
-            for(int j = 0; j < answer[i].length; j++){
-                answer[i][j] += arr2[i][j];
+        int[][] answer = new int[arr1.length][arr1[0].length];
+        
+        for(int i = 0; i < arr1.length; i++){
+            for (int j = 0; j < arr1[0].length; j++){
+                answer[i][j] += arr1[i][j] + arr2[i][j];
             }
         }
+        
         return answer;
     }
 }
