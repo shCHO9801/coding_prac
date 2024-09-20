11ccffde0954431ff1e5b187ec29ef48b10f2a45
shCHO9801 <csh980116@naver.com>
2024-09-20 18:31:12 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 28.49 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/README.md
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/README.md
@@ -1,10 +1,10 @@
 # [level 1] 크기가 작은 부분문자열 - 147355 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/147355) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/147355?language=java) 
 
 ### 성능 요약
 
-메모리: 72.1 MB, 시간: 2.03 ms
+메모리: 91.7 MB, 시간: 28.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 10일 15:48:19
+2024년 09월 20일 18:31:07
 
 ### 문제 설명
 
--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -1,10 +1,21 @@
 class Solution {
+    public static int func(String tmp, String p){
+        return Long.parseLong(tmp) <= Long.parseLong(p) ? 1 : 0;
+    }
+    
     public int solution(String t, String p) {
-        int answer = 0;
-        for(int i = 0; i < t.length() - p.length() + 1; i++){
-            String tmp = t.substring(i, i+p.length());
-            if(p.compareTo(tmp) >= 0) answer++;
+        if(t.length() == 1 || (t.length() == p.length())){
+            return func(t, p);
         }
+        int answer = 0;
+        int len = p.length();
+        
+        for(int i = 0; i <= t.length() - len; i++){
+            String tmp = t.substring(i,i + len);
+            System.out.println(tmp);
+            answer += func(tmp, p);
+        }
+        
         return answer;
     }
 }
