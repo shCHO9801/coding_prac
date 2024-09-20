7d911349c1dc9eadcfca8665305dc8e7f1db60ed
shCHO9801 <csh980116@naver.com>
2024-09-20 12:33:45 +0900

    [level 1] Title: 체육복, Time: 0.04 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,11 +1,11 @@
 import java.util.*;
+
 class Solution {
-    public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
-        int[] arr = new int[n + 1];
-        ArrayList <Integer> tmp = new ArrayList<>();
-        
+    public static int[] setStudents(int n, int[] lost, int[] reserve){
+        int[] arr = new int[n + 2];
         Arrays.fill(arr, 1);
+        arr[0] = 0;
+        arr[n + 1] = 0;
         
         for(int l : lost){
             arr[l]--;
@@ -15,24 +15,31 @@ class Solution {
             arr[r]++;
         }
         
+        return arr;
+    }
+    
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        int[] students = setStudents(n, lost, reserve);
+        
         for(int i = 1; i <= n; i++){
-            if(arr[i] == 0){
-                if(i > 1 && arr[i - 1] > 1){
-                    arr[i]++;
-                    arr[i - 1]--;
-                }
-                else if(i < n && arr[i + 1] > 1){
-                    arr[i]++;
-                    arr[i + 1]--;
+            if(students[i] == 0){
+                if(students[i - 1] >= 2){
+                    students[i - 1]--;
+                    students[i]++;
+                } else if(students[i + 1] >= 2){
+                    students[i + 1]--;
+                    students[i]++;
                 }
             }
         }
         
         for(int i = 1; i <= n; i++){
-            if(arr[i] > 0){
+            if(students[i] >= 1){
                 answer++;
             }
         }
+        
         return answer;
     }
 }
