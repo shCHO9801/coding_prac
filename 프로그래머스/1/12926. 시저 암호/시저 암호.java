f6f39e9a26897a180cd61a7480d9196ff2eb85a8
shCHO9801 <csh980116@naver.com>
2024-09-22 00:00:50 +0900

    [level 1] Title: 시저 암호, Time: 1.75 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,21 +1,18 @@
 class Solution {
     public String solution(String s, int n) {
-        String answer = "";
-        for(int i = 0; i < s.length();i++){
-            char tmp = s.charAt(i);
-            int c = tmp + n;
-            if(Character.isLowerCase(tmp)){
-                if(c > 'z')
-                    c-=26;
+        StringBuilder answer = new StringBuilder();
+        
+        for(char c : s.toCharArray()){
+            int tmp = c + n;
+            if(c == ' '){
+                tmp = ' ';
+            } else if (Character.isLowerCase(c) && tmp > 'z'){
+                tmp -= 26;
+            } else if (Character.isUpperCase(c) && tmp > 'Z'){
+                tmp -= 26;
             }
-            else if(Character.isUpperCase(tmp)){
-                if(c > 'Z')
-                    c-=26;
-            }
-            else
-                c = ' ';
-            answer+=String.valueOf((char)c);
+            answer.append((char)tmp);
         }
-        return answer;
+        return answer.toString();
     }
 }
