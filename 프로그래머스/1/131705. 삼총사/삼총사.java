f37d53d5bce95f29854bf302bd22a6e2b37cc240
shCHO9801 <csh980116@naver.com>
2024-09-21 23:51:22 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -2,11 +2,11 @@ class Solution {
     public int solution(int[] number) {
         int answer = 0;
         for(int i = 0; i < number.length-2; i++){
-            for(int j = i + 1; j < number.length-1; j++){
+            for(int j = i + 1; j < number.length - 1; j++){
                 for(int k = j + 1; k < number.length; k++){
-                    int tmp = number[i] + number[j] + number[k];
-                    if(tmp == 0)
+                    if(number[i] + number[j] + number[k] == 0){
                         answer++;
+                    }
                 }
             }
         }
