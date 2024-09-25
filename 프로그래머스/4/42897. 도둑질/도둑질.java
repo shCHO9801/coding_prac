2dc3933b75d52fd38dd78d7d69bf49acb370c409
shCHO9801 <csh980116@naver.com>
2024-09-25 12:35:13 +0900

    [level 4] Title: 도둑질, Time: 28.85 ms, Memory: 98.4 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -1,22 +1,25 @@
+import java.util.*;
 class Solution {
     public int solution(int[] money) {
         int n = money.length;
+        int answer = 0;
         
-        int[] dp1 = new int[n];
-        dp1[0] = money[0];
-        dp1[1] = Math.max(money[0], money[1]);
+        int[] dp = new int[n];
+        dp[0] = money[0];
+        dp[1] = Math.max(money[0], money[1]);
         
         for(int i = 2; i < n - 1; i++){
-            dp1[i] = Math.max(dp1[i - 1], dp1[i - 2] + money[i]);
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + money[i]);
         }
         
-        int[] dp2 = new int[n];
-        dp2[0] = 0;
-        dp2[1] = money[1];
+        answer = dp[n - 2];
+        Arrays.fill(dp, 0);
+        
+        dp[1] = money[1];
         
         for(int i = 2; i < n; i++){
-            dp2[i] = Math.max(dp2[i - 1], dp2[i - 2] + money[i]);
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + money[i]);
         }
-        return Math.max(dp1[n - 2], dp2[n - 1]);
+        return Math.max(answer, dp[n - 1]);
     }
 }
