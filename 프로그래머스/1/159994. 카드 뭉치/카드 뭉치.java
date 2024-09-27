69d1fe64c1ef022b6f3b712acb8c4ca68f78ce9b
shCHO9801 <csh980116@naver.com>
2024-09-27 18:43:21 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,20 +1,20 @@
+import java.util.*;
+
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        boolean check = true;
+        
         int idx1 = 0;
         int idx2 = 0;
+        
         for(String s : goal){
-            if(idx1 < cards1.length && s.equals(cards1[idx1])){
+            if(idx1 < cards1.length && cards1[idx1].equals(s)){
                 idx1++;
-            }
-            else if(idx2 < cards2.length && s.equals(cards2[idx2])){
+            } else if (idx2 < cards2.length && cards2[idx2].equals(s)) {
                 idx2++;
-            }
-            else{
-                check = false;
-                break;
+            } else{
+                return "No";
             }
         }
-        return check ? "Yes" : "No";
+        return "Yes";
     }
 }
