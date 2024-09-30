2c9eea1cae4080c2e418ef6ce43fdc36f5bec267
shCHO9801 <csh980116@naver.com>
2024-10-01 00:01:57 +0900

    [level 1] Title: 덧칠하기, Time: 1.81 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,14 +1,16 @@
 import java.util.*;
 class Solution {
     public int solution(int n, int m, int[] section) {
-        int answer = 1;
-        int cur = section[0] + m - 1;
-        for(int i = 1; i < section.length; i++){
-            if(cur < section[i]){
+        int answer = 0;
+        int cur = 0;
+        
+        for(int i : section){
+            if(cur < i){
                 answer++;
-                cur = section[i] + m - 1;
+                cur = i + m - 1;    
             }
         }
+        
         return answer;
     }
 }
