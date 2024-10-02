a7020b641369314c4b126036a66c35cac7166daa
shCHO9801 <csh980116@naver.com>
2024-10-02 20:40:07 +0900

    [level 1] Title: 예산, Time: 0.99 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -3,16 +3,17 @@ import java.util.*;
 class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
-        int count = 0;
+        int sum = 0;
         Arrays.sort(d);
         
-        for(int i = 0; i < d.length; i++){
-            if(count + d[i] > budget){
+        for(int i : d){
+            if(sum + i > budget){
                 break;
             }
-            count += d[i];
+            sum += i;
             answer++;
         }
+        
         return answer;
     }
 }
