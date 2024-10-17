71fdc8afa1716ce8b73dff100a0b4bbdd60172fa
shCHO9801 <csh980116@naver.com>
2024-10-18 00:08:10 +0900

    [level 1] Title: 명예의 전당 (1), Time: 2.42 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,21 +1,19 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
-        ArrayList<Integer> list = new ArrayList<>();
+        PriorityQueue <Integer> pq = new PriorityQueue<>();
         int idx = 0;
-        for(int i : score){
-            list.add(i);
-            Collections.sort(list, (x,y) -> {
-               return y-x; 
-            });
-            if(list.size() < k){
-                answer[idx++] = (int)list.get(list.size() - 1);
+        
+        for(int s : score){
+            pq.add(s);
+            while(pq.size() > k){
+                pq.poll();
             }
-            else{
-                answer[idx++] = (int)list.get(k - 1);
-            }
+            answer[idx++] = pq.peek();
         }
+        
         
         return answer;
     }
