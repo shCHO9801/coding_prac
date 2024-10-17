8991bb1ade36ea76537db1413ee005e01ffc8733
shCHO9801 <csh980116@naver.com>
2024-10-18 00:02:20 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.16 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,36 +1,43 @@
+import java.util.*;
+
 class Solution {
-    static String makeBinary(int n, int len){
-        String tmp = "";
-        String result = "";
-        while(n!=0){
-            tmp += String.valueOf(n%2);
-            n/=2;
+    public static String getBin(int n, int a){
+        StringBuilder result = new StringBuilder();
+        
+        String s = Integer.toString(a, 2);
+        if(s.length() < n){
+            int need = n - s.length();
+            while(need-- > 0){
+                result.append('0');
+            }
         }
-        while(tmp.length() < len)
-            tmp+="0";
-        for(int i = tmp.length() - 1; i >= 0; i--){
-            result += tmp.charAt(i);
+        
+        return result.append(s).toString();
+    }
+    
+    public static String makeString(int n, int a, int b){
+        StringBuilder result = new StringBuilder();
+        
+        char[] s1 = getBin(n, a).toCharArray();
+        char[] s2 = getBin(n, b).toCharArray();
+        
+        for(int i = 0; i < n; i++){
+            if(s1[i] == '1' || s2[i] == '1'){
+                result.append('#');
+            } else {
+                result.append(' ');
+            }
         }
-        return result;
+        
+        return result.toString();
     }
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String [n];
-        for(int i = 0; i < arr1.length; i++){
-            answer[i] = makeBinary(arr1[i], n);
+        String[] answer = new String[n];
+        
+        for(int i = 0; i < n; i++){
+            answer[i] = makeString(n, arr1[i], arr2[i]);
         }
-        for(int i = 0; i < arr2.length; i++){
-            String tmp = makeBinary(arr2[i], n);
-            String result = "";
-            for(int j = 0; j < answer.length; j++){
-                if(tmp.charAt(j) == '1' || answer[i].charAt(j) == '1'){
-                    result += '#';
-                }
-                else{
-                    result += ' ';
-                }
-            }
-            answer[i] = result;
-        }
+        
         return answer;
     }
 }
