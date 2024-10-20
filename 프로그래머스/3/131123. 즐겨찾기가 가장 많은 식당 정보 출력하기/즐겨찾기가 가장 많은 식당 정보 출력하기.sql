2d4b18388037bc5a17440455eb431e457191ea3d
shCHO9801 <csh980116@naver.com>
2024-10-21 01:43:54 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -0,0 +1,13 @@
+
+
+select food_type, rest_id, rest_name, favorites
+from rest_info
+where (food_type, favorites)
+in
+(
+    select food_type, max(favorites) as favorites
+    from rest_info
+    group by food_type
+)
+order by 1 desc
+
