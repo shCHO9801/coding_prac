183cb31fc51d56a4a0b302b4a69bc741af6160e5
shCHO9801 <csh980116@naver.com>
2024-10-21 02:21:43 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.30 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -1,42 +1,40 @@
+import java.util.*;
+
 class Solution {
-    static int[] score = new int[3];
-    static int idx = 0;
-    static void makeScore(String curScore, String s){
-        int result = Integer.parseInt(curScore);
-        score[idx] = result;
-        if(s.equals("D")){
-            score[idx] = (int)Math.pow(result, 2);
-        }
-        else if(s.equals("T")){
-            score[idx] = (int)Math.pow(result, 3);
-        }
-        idx++;
-    }
-    
     public int solution(String dartResult) {
-        int answer = 0;    
-        String curScore = "";
-        for(String s : dartResult.split("")){
-            if(s.equals("*")){
-                if(idx - 2 >= 0){
-                    score[idx -2] *= 2;
+        int answer = 0;
+        Stack <Integer> score = new Stack<>();
+        StringBuilder tmp = new StringBuilder();
+        
+        for(char c : dartResult.toCharArray()){
+            if(c == '*'){
+                int t = score.pop() * 2;
+                if(!score.isEmpty()){
+                    score.push(score.pop() * 2);
                 }
-                score[idx - 1] *= 2;
-            }
-            else if(s.equals("#")){
-                score[idx - 1] *= -1;
-            }
-            else if(s.equals("S") || s.equals("D") || s.equals("T")){
-                makeScore(curScore, s);
-                curScore = "";
-            }
-            else{
-                curScore += s;
+                score.push(t);
+            } else if (c == '#'){
+                score.push(score.pop() * -1);
+            } else if (c == 'S'){
+                score.push(Integer.parseInt(tmp.toString()));
+                tmp = new StringBuilder();
+            } else if (c == 'D'){
+                int t = (int)Math.pow(Integer.parseInt(tmp.toString()), 2);
+                score.push(t);
+                tmp = new StringBuilder();
+            } else if (c == 'T'){
+                int t = (int)Math.pow(Integer.parseInt(tmp.toString()), 3);
+                score.push(t);
+                tmp = new StringBuilder();
+            } else {
+                tmp.append(c - '0');
             }
         }
-        for(int i = 0; i < 3; i++){
-            answer += score[i];
+        
+        while(!score.isEmpty()){
+            answer += score.pop();
         }
+        
         return answer;
     }
 }
