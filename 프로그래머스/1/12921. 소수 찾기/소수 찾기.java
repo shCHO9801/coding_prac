b33666492473818e1c69fa79192184ac7e313f3a
shCHO9801 <csh980116@naver.com>
2024-10-21 01:10:46 +0900

    [level 1] Title: 소수 찾기, Time: 286.16 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -1,19 +1,14 @@
 class Solution {
-    static boolean isPrime(int n){
-        if(n < 2) return false;
-        if(n == 2) return true;
-        if(n % 2 == 0) return false;
-        
-        for(int i = 3; i <= Math.sqrt(n); i++){
-            if(n % i == 0){
-                return false;
-            }
+    public static boolean isPrime(int n){
+        for(int i = 2; i * i <= n; i++){
+            if(n % i == 0) return false;
         }
         return true;
     }
+    
     public int solution(int n) {
         int answer = 0;
-        for(int i = 1; i <= n; i++){
+        for(int i = 2; i <= n; i++){
             if(isPrime(i)){
                 answer++;
             }
