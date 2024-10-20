efc744e95d1bcd6e67a6b01c3010fbb74b29a06f
shCHO9801 <csh980116@naver.com>
2024-10-21 01:02:41 +0900

    [level 1] Title: 소수 만들기, Time: 6.28 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,32 +1,32 @@
-import java.util.*;
 class Solution {
-    static boolean[] memo = new boolean[4000];
-    static boolean isPrime(int n){
-        if(n == 1 || n == 2)
-            return false;
-        if(memo[n])
-            return true;
-        int i = 2;
-        for(; i < n; i++){
-            if(n % i == 0){
-                break;
-            }
+    static boolean[] memo;
+    public static boolean isPrime(int n){
+        if (memo[n]){
+            return memo[n];
         }
-        
-        if(i == n){
+        if(n == 2){
             return memo[n] = true;
         }
-        return false;
+        if(n % 2 == 0){
+            return false;
+        }
+        
+        for(int i = 2; i < n; i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return memo[n] = true;
+        
     }
     public int solution(int[] nums) {
         int answer = 0;
-        Arrays.fill(memo, false);
-        Arrays.sort(nums);
+        memo = new boolean[3001];
+        
         for(int i = 0; i < nums.length - 2; i++){
-            for(int j = i + 1; j < nums.length - 1; j++){
+            for (int j = i + 1; j < nums.length - 1; j++){
                 for(int k = j + 1; k < nums.length; k++){
-                    int num = nums[i] + nums[j] + nums[k];
-                    if(isPrime(num)){
+                    if(isPrime(nums[i] + nums[j] + nums[k])){
                         answer++;
                     }
                 }
