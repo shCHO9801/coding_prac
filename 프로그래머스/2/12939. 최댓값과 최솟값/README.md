b945bb86500078db9d3e206fd2a987bc96dafc27
shCHO9801 <csh980116@naver.com>
2024-10-22 00:48:42 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 10.71 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/README.md
+++ 프로그래머스/2/12939. 최댓값과 최솟값/README.md
@@ -1,10 +1,10 @@
 # [level 2] 최댓값과 최솟값 - 12939 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939?language=java) 
 
 ### 성능 요약
 
-메모리: 78.6 MB, 시간: 10.78 ms
+메모리: 82.8 MB, 시간: 10.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 10일 17:18:45
+2024년 10월 22일 00:48:39
 
 ### 문제 설명
 
--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,14 +1,15 @@
 class Solution {
     public String solution(String s) {
-        StringBuilder answer = new StringBuilder();
-        String[] tmp = s.split(" ");
-        int max = Integer.parseInt(tmp[0]);
-        int min = Integer.parseInt(tmp[0]);
-        for(String t : tmp){
-            max = Math.max(max, Integer.parseInt(t));
-            min = Math.min(min, Integer.parseInt(t));
+        String answer = "";
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        
+        for(String tmp : s.split(" ")){
+            int t = Integer.parseInt(tmp);
+            min = Math.min(min, t);
+            max = Math.max(max, t);
         }
-        answer.append(min + " " + max);
-        return answer.toString();
+        
+        return min + " " + max;
     }
 }
