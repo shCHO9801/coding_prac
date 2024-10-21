37998d7af266fdea28cbecfc715f9c63ad42f3e9
shCHO9801 <csh980116@naver.com>
2024-10-22 00:45:36 +0900

    [level 2] Title: 올바른 괄호, Time: 51.66 ms, Memory: 58.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,23 +1,17 @@
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-        int left = 0;
-        for(int i = 0; i < s.length(); i++){
-            char x = s.charAt(i);
-            if(x == ')'){
-                if(left==0){
-                    answer = false;
-                    break;
+        int count = 0;
+        for(String tmp : s.split("")){
+            if(tmp.equals("(")){
+                count++;
+            } else {
+                if(count <= 0){
+                    return false;
+                } else {
+                    count--;
                 }
-                else{
-                    left--;
-                }
-            }
-            else{
-                left++;
             }
         }
-        if(left!=0) answer = false;
-        return answer;
+        return count == 0;
     }
 }
