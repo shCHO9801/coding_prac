20fcb132af0d7f3f71d5ff17805875969df869ef
shCHO9801 <csh980116@naver.com>
2024-11-05 16:45:55 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 516 ms, Memory: 52684 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/README.md
+++ 백준/Gold/11729. 하노이 탑 이동 순서/README.md
@@ -4,7 +4,7 @@
 
 ### 성능 요약
 
-메모리: 51616 KB, 시간: 588 ms
+메모리: 52684 KB, 시간: 516 ms
 
 ### 분류
 
@@ -12,7 +12,7 @@
 
 ### 제출 일자
 
-2024년 11월 5일 16:41:31
+2024년 11월 5일 16:45:44
 
 ### 문제 설명
 
@@ -27,7 +27,7 @@
 
 <p>아래 그림은 원판이 5개인 경우의 예시이다.</p>
 
-<p style="text-align: center;"><img alt="" src="" style="height:200px; width:1050px"></p>
+<p style="text-align: center;"><img alt="" src="https://onlinejudgeimages.s3-ap-northeast-1.amazonaws.com/problem/11729/hanoi.png" style="height:200px; width:1050px"></p>
 
 ### 입력 
 
--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -3,30 +3,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static int count = 0;
     static StringBuilder answer;
-
-    private static void addAnswer(int n1, int n2) {
-        count++;
-        answer.append(n1).append(" ").append(n2).append("\n");
-    }
-
-    private static void hanoi(int n, int start, int tmp, int to) {
-        if (n == 1) {
-            addAnswer(start, to);
-            return;
-        }
-
-        hanoi(n - 1, start, to, tmp);
-        addAnswer(start, to);
-        hanoi(n - 1, tmp, start, to);
-    }
+    static int count;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         answer = new StringBuilder();
         int k = Integer.parseInt(br.readLine());
+
         hanoi(k, 1, 2, 3);
         System.out.println(count + "\n" + answer);
+    }
+
+    private static void hanoi(int n, int start, int mid, int end) {
+        if (n == 1) {
+            // start -> end
+            addAnswer(start, end);
+            return;
+        }
+        hanoi(n - 1, start, end, mid);
+        // start -> end
+        addAnswer(start, end);
+        hanoi(n - 1, mid, start, end);
+    }
+
+    private static void addAnswer(int start, int end) {
+        count++;
+        answer.append(start).append(" ").append(end).append("\n");
     }
 }
