8dc7ac43d9b7b50b2e5769ac58d2db81d534d710
shCHO9801 <csh980116@naver.com>
2024-12-20 01:17:02 +0900

    [level 2] Title: 전화번호 목록, Time: 286.35 ms, Memory: 223 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,11 +2,20 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book);
         
-        for(int i = 0; i < phone_book.length - 1; i++){
-            if(phone_book[i + 1].startsWith(phone_book[i]))
-                return false;
+        Set <String> set = new HashSet<>();
+        
+        for(String p : phone_book) {
+            set.add(p);
+        }
+        
+        for(String p : phone_book) {
+            for(int i = 0; i < p.length(); i++){
+                String tmp = p.substring(0, i);
+                if(set.contains(tmp)) {
+                    return false;
+                }
+            }
         }
         
         return true;
