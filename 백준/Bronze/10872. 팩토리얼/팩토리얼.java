f65cc0924f3906bfd09a0d652baadd817f983b46
shCHO9801 <csh980116@naver.com>
2024-12-24 01:35:52 +0900

    [Bronze III] Title: 팩토리얼, Time: 104 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -1,13 +1,15 @@
-import java.util.*;
 import java.io.*;
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        long answer = 1;
-        for (int i = 1; i <= n; i++) {
-            answer*=i;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(f(n));
+    }
+    private static int f(int depth) {
+        if(depth <= 1) {
+            return 1;
         }
-        System.out.println(answer);
+        return depth * f(depth - 1);
     }
 }
