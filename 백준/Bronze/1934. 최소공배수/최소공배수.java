f8c4005130e07d2241598b68380c3041ba40ba65
shCHO9801 <csh980116@naver.com>
2024-12-25 00:06:48 +0900

    [Bronze I] Title: 최소공배수, Time: 120 ms, Memory: 14564 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -2,27 +2,38 @@ import java.util.*;
 import java.io.*;
 public class Main{
     static StringTokenizer st;
-    static int T,a,b;
-    public static int get(int a, int b){
-        int idx = 2;
-        int c = 1;
-        while(Math.max(a,b) >= idx){
-            while(a%idx == 0 && b%idx == 0){
-                c*=idx;
-                a/=idx;b/=idx;
-            }
-            idx++;
-        }
-        return c * a * b;
-    }
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        T = Integer.parseInt(br.readLine());
-        while(T-->0){
+        StringBuilder answer = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
+
+        while(t-- > 0) {
             st = new StringTokenizer(br.readLine());
-            a = Integer.parseInt(st.nextToken());
-            b = Integer.parseInt(st.nextToken());
-            System.out.println(get(a,b));
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            answer.append(lcm(a, b)).append('\n');
         }
+
+        System.out.println(answer);
     }
+
+    private static int lcm(int a, int b) {
+        int gcd = gcd(a, b);
+        a /= gcd;
+        b /= gcd;
+        return gcd * a * b;
+    }
+
+    private static int gcd(int a, int b) {
+        if(a < b) {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+        if(b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
 }
