b8ab569601315a6e5315fde9778abc33a84976e4
shCHO9801 <csh980116@naver.com>
2025-01-02 21:26:33 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 19312 ms, Memory: 130312 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder answer = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(list);
+
+        for(int i : list) {
+            answer.append(i).append('\n');
+        }
+
+        System.out.println(answer);
+    }
+}
