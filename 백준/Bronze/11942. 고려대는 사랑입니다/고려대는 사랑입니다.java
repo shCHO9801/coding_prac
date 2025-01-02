3311430e6888733b4698168ada171b77c107cf93
shCHO9801 <csh980116@naver.com>
2025-01-02 22:55:24 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 100 ms, Memory: 13920 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        System.out.println("고려대학교");
+    }
+}
