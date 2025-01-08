262940c18108b0857242d67e4e7ef4549e81fd92
shCHO9801 <csh980116@naver.com>
2025-01-08 18:54:14 +0900

    [Silver II] Title: N과 M (9), Time: 192 ms, Memory: 26744 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -7,21 +7,20 @@ public class Main {
     static boolean[] visited;
     static int n;
     static int m;
+    static StringBuilder answer;
+    static List<Integer> tmp;
 
     public static void main(String[] args) throws IOException {
-        StringBuilder answer = new StringBuilder();
-        List<Integer> tmp = new ArrayList<>();
-
         setUp();
-        backTracking(m,  tmp, answer);
+        backTracking(m, tmp, answer);
 
         System.out.println(answer);
     }
 
     private static void backTracking(int m, List<Integer> tmp, StringBuilder answer) {
         if(m == tmp.size()) {
-            for (int i = 0; i < m; i++) {
-                answer.append(tmp.get((i)));
+            for(int i = 0; i < m; i++) {
+                answer.append(tmp.get(i));
                 if(i < m - 1) {
                     answer.append(' ');
                 }
@@ -30,24 +29,22 @@ public class Main {
             return;
         }
         for (int i = 0; i < n; i++) {
-            if(visited[i]) {
-                continue;
-            }
-            if(i > 0 && arr[i - 1] == arr[i] && !visited[i - 1]) {
-                continue;
-            }
+            if(visited[i]) continue;
+            if(i > 0 && arr[i - 1] == arr[i] && !visited[i - 1]) continue;
             tmp.add(arr[i]);
             visited[i] = true;
 
             backTracking(m, tmp, answer);
 
-            visited[i] = false;
             tmp.remove(tmp.size() - 1);
+            visited[i] = false;
         }
     }
 
     private static void setUp() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        answer = new StringBuilder();
+        tmp = new ArrayList<>();
 
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
@@ -55,12 +52,11 @@ public class Main {
 
         arr = new int[n];
         visited = new boolean[n];
-
         st = new StringTokenizer(br.readLine());
+
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
     }
-
 }
