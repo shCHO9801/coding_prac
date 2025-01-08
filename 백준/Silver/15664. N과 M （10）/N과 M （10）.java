9f86b14ebb50ed8ee0e9fd25f9e5fe1fb62ee7ae
shCHO9801 <csh980116@naver.com>
2025-01-08 18:19:41 +0900

    [Silver II] Title: N과 M (10), Time: 100 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static StringTokenizer st;
+    static int[] arr;
+    static boolean[] visited;
+    static int n;
+    static int m;
+
+    public static void main(String[] args) throws IOException {
+        StringBuilder answer = new StringBuilder();
+        List<Integer> tmp = new ArrayList<>();
+
+        setUp();
+        backTracking(0, m,  tmp, answer);
+
+        System.out.println(answer);
+    }
+
+    private static void backTracking(int start, int m, List<Integer> tmp, StringBuilder answer) {
+        if(m == tmp.size()) {
+            for (int i = 0; i < m; i++) {
+                answer.append(tmp.get((i)));
+                if(i < m - 1) {
+                    answer.append(' ');
+                }
+            }
+            answer.append('\n');
+            return;
+        }
+        for (int i = start; i < n; i++) {
+            if(visited[i]) {
+                continue;
+            }
+            if(i > 0 && arr[i - 1] == arr[i] && !visited[i - 1]) {
+                continue;
+            }
+            tmp.add(arr[i]);
+            visited[i] = true;
+
+            backTracking(i + 1, m, tmp, answer);
+
+            visited[i] = false;
+            tmp.remove(tmp.size() - 1);
+        }
+    }
+
+    private static void setUp() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        visited = new boolean[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+    }
+
+}
