af119aa43c14e42512b7e5fa36b288fb40b31838
shCHO9801 <csh980116@naver.com>
2025-01-15 21:21:57 +0900

    [Bronze I] Title: 나무 조각, Time: 100 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Bronze/2947. 나무 조각/나무 조각.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder answer = new StringBuilder();
+        final int n = 5;
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                if(arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                    for(int k : arr) {
+                        answer.append(k).append(' ');
+                    }
+                    answer.append('\n');
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
