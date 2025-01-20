cc9dae815f46265226e8d23ad0ca2b93b860be05
shCHO9801 <csh980116@naver.com>
2025-01-20 16:03:17 +0900

    [Bronze V] Title: We love kriii, Time: 104 ms, Memory: 14076 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
