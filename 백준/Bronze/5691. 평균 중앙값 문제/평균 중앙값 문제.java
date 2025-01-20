6d53edb58111da353e273fa5fc1ccf1464873f85
shCHO9801 <csh980116@naver.com>
2025-01-20 11:27:32 +0900

    [Bronze III] Title: 평균 중앙값 문제, Time: 104 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Bronze/5691. 평균 중앙값 문제/평균 중앙값 문제.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder answer = new StringBuilder();
+        
+        while(true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if(a == 0 && b == 0) break;
+
+            answer.append(a - (b - a)).append('\n');
+        }
+
+        System.out.println(answer);
+    }
+}
