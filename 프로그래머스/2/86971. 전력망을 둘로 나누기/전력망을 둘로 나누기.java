495a84a631324eb8715a122d647c058fa2193b55
sunghyunCho <csh980116@naver.com>
2026-03-22 23:59:30 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 11.48 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -4,18 +4,17 @@ class Solution {
         
         for(int i = 0; i < n; i++) {
             boolean[] visited = new boolean[n + 1];
-            int count = dfs(1, i, wires, visited);
-            
-            int diff = Math.abs(count - (n - count));
-            answer = Math.min(diff, answer);
+            int count = dfs(1, i, visited, wires);
+            int diff = Math.abs((n - count) - count);
+            answer = Math.min(answer, diff);
         }
         
         return answer;
     }
-
-    private int dfs(int node, int skip, int[][] wires, boolean[] visited) {
-        visited[node] = true;
+    
+    private int dfs(int node, int skip, boolean[] visited, int[][] wires) {
         int count = 1;
+        visited[node] = true;
         
         for(int i = 0; i < wires.length; i++) {
             if(i == skip) continue;
@@ -23,10 +22,10 @@ class Solution {
             int a = wires[i][0];
             int b = wires[i][1];
             
-            if (node == a && !visited[b]) {
-                count += dfs(b, skip, wires, visited);
-            } else if(node == b && !visited[a]) {
-                count += dfs(a, skip, wires, visited);
+            if(a == node && !visited[b]) {
+                count += dfs(b, skip, visited, wires);
+            } else if(b == node && !visited[a]) {
+                count += dfs(a, skip, visited, wires);
             }
         }
         
