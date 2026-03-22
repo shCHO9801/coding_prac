0fe235a506065ebb3ead08ad6f2129b37bf03a62
sunghyunCho <csh980116@naver.com>
2026-03-22 23:09:21 +0900

    [level 3] Title: 여행경로, Time: 75.01 ms, Memory: 116 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    List<String> list = new ArrayList<>();
+    
+    public String[] solution(String[][] tickets) {
+        String[] answer = {};
+            
+        boolean[] visited = new boolean[tickets.length];
+        dfs("ICN", "ICN ", 0, tickets, tickets.length, visited);
+        
+        Collections.sort(list);
+        
+        return list.get(0).split(" ");
+    }
+    
+    private void dfs(String cur, String s, int count, String[][] tickets, int totalTicket, boolean[] visited) {  
+        if(totalTicket == count) {
+            list.add(s);
+            return;
+        }
+        
+        for(int i = 0; i < tickets.length; i++) {
+            if(tickets[i][0].equals(cur) && !visited[i]) {
+                visited[i] = true;
+                
+                dfs(tickets[i][1], s + tickets[i][1] + " ", count + 1, tickets, totalTicket, visited);
+                
+                visited[i] = false;
+            }
+        }
+    }
+}
