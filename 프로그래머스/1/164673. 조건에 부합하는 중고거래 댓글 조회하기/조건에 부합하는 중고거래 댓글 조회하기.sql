e0a9f76ec176961b710a0bf78d62bfd648e79126
sunghyunCho <csh980116@naver.com>
2026-03-26 16:40:30 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,10 +1,11 @@
-select b.title, b.board_id, r.reply_id, r.writer_id, r.contents, 
-date_format(r.created_date, '%Y-%m-%d') as created_date
-from (
-    select board_id, title, writer_id
-    from used_goods_board
-    where created_date like '2022-10%'
-) b left join used_goods_reply r
-on b.board_id = r.board_id
-where r.reply_id is not null
-order by created_date, title
+SELECT 
+    ugb.title as title,
+    ugb.board_id as board_id,
+    ugr.reply_id as reply_id,
+    ugr.writer_id as writer_id,
+    ugr.contents as contents,
+    date_format(ugr.created_date, '%Y-%m-%d') as created_date
+FROM USED_GOODS_BOARD ugb
+    JOIN USED_GOODS_REPLY ugr ON ugb.board_id = ugr.board_id
+WHERE ugb.created_date BETWEEN '2022-10-01' AND '2022-10-31'
+ORDER BY ugr.created_date asc, ugb.title asc
