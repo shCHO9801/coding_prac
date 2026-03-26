8d2d8a4bfa053e052b63260e08258af9942254b0
sunghyunCho <csh980116@naver.com>
2026-03-26 16:12:28 +0900

    [level 1] Title: 12세 이하인 여자 환자 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/132201. 12세 이하인 여자 환자 목록 출력하기/12세 이하인 여자 환자 목록 출력하기.sql
+++ 프로그래머스/1/132201. 12세 이하인 여자 환자 목록 출력하기/12세 이하인 여자 환자 목록 출력하기.sql
@@ -1,9 +1,6 @@
-select pt_name, pt_no, gend_cd, age, 
-case when tlno is null 
-    then 'NONE'
-    else tlno
-    end as tlno
-from patient
-where age <= 12
-and gend_cd = 'W'
-order by age desc, pt_name;
+SELECT pt_name, pt_no, gend_cd, age,
+CASE WHEN tlno is NULL
+then 'NONE' ELSE tlno END as tlno
+FROM patient
+WHERE age <= 12 and gend_cd = 'W'
+ORDER BY age desc, pt_name
