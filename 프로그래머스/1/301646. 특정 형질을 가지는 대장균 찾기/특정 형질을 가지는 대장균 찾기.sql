a50344b84533107a258761a5f9c0451784ccff4c
sunghyunCho <csh980116@naver.com>
2026-03-27 14:50:07 +0900

    [level 1] Title: 특정 형질을 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++ 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
@@ -1,7 +1,4 @@
-select count(*) as count
-from (
-    select conv(genotype, 10, 2) as genotype
-    from ecoli_data
-    where conv(genotype, 10, 2) not like '%1_'
-) as ecoli_data
-where genotype like '%1' or genotype like '%1__'
+SELECT count(*) as count
+FROM ecoli_data
+WHERE genotype & 2 = 0
+AND (genotype & 1 != 0 OR genotype & 4 != 0)
