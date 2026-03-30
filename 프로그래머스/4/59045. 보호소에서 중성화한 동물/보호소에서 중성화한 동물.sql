a3070557ad6e212f5c6b209895deeb094d8aff1f
sunghyunCho <csh980116@naver.com>
2026-03-30 19:55:48 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,10 @@
+SELECT
+    i.animal_id,
+    i.animal_type,
+    i.name
+FROM animal_ins i
+JOIN animal_outs o 
+    ON i.animal_id = o.animal_id
+WHERE i.SEX_UPON_INTAKE LIKE 'Intact%'
+    AND (o.SEX_UPON_OUTCOME LIKE 'Spayed%' OR o.SEX_UPON_OUTCOME LIKE 'Neutered%')
+ORDER BY i.animal_id asc;
